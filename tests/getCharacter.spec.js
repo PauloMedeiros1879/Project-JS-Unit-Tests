924c1075f5f8015dffed9238eb6017b5d2255a13
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-09 14:38:54 -0400

    Adiciona requisito 9 e correçoes

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -45,12 +45,54 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    expect(getCharacter([])).toBeUndefined();
+    expect(getCharacter()).toBeFalsy();
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    expect(getCharacter('Arya').length).toBe(1)
+    expect(getCharacter('Arya')).toEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'],
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('brienne')).toEqual({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: [
+        'Im No Lady, Your Grace.',
+        'I, Brienne Of Tarth, Sentence You To Die.',
+      ],
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('melissandre')).toEqual({
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: [
+        'Death By Fire Is The Purest Death.',
+        'For The Night Is Dark And Full Of Terrors.',
+      ],
+    });
     // Teste se os parâmetros não são Case Sensitive.
+    expect(getCharacter('aRYa')).toEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'],
+    });
+    expect(getCharacter('bRIeNne')).toEqual({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: [
+        'Im No Lady, Your Grace.',
+        'I, Brienne Of Tarth, Sentence You To Die.',
+      ],
+    });
+    expect(getCharacter('MeLisSaNdRe')).toEqual({
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: [
+        'Death By Fire Is The Purest Death.',
+        'For The Night Is Dark And Full Of Terrors.',
+      ],
+    });
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('GameOfThrones')).toBeUndefined();
   });
 });
